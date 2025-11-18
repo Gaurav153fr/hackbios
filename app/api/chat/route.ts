364bf347b2db2925bba205e87e019280e4f015d1
Gaurav153fr <118659860+Gaurav153fr@users.noreply.github.com>
2025-11-18 12:37:56 +0530

    fixed another error

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,17 +1,17 @@
-import { GoogleGenerativeAI } from "@google/generative-ai";
-import { streamText, StreamingTextResponse } from "ai";
+// import { GoogleGenerativeAI } from "@google/generative-ai";
+// import { streamText, StreamingTextResponse } from "ai";
 
-export const runtime = "edge";
+// export const runtime = "edge";
 
-export async function POST(req: Request) {
-  const { messages } = await req.json();
+// export async function POST(req: Request) {
+//   const { messages } = await req.json();
 
-  const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
+//   const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
 
-  const response = await streamText({
-    model: genAI.getGenerativeModel({ model: "gemini-2.0-flash" }), // change model here
-    messages,
-  });
+//   const response = await streamText({
+//     model: genAI.getGenerativeModel({ model: "gemini-2.0-flash" }), // change model here
+//     messages,
+//   });
 
-  return new StreamingTextResponse(response);
-}
+//   return new StreamingTextResponse(response);
+// }
